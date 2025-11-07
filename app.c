56849cb520ad2cc56b8763b6cafd1fed0fd6e3b9
Aishwarya <ravichandranaishwarya89@gmail.com>
2025-11-07 06:54:13 +0100

    Changes Made2

--- app.c
+++ app.c
@@ -3,15 +3,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 #define FILENAME "/dev/file1"
+#define FILENAME "/dev/file1"
 int main() {
     int fd = open(FILENAME, O_RDWR);
-
     const char *buff = "Hello Tessolve";
     read(fd,buff,sizeof(buff)-1);
     write(fd, buff, strlen(buff));
     printf("Line added");
-
-   
 //    printf("Read from device: %s\n", buff);
 
     close(fd);
